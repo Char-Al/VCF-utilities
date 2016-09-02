63aa89b3004b4900f280e61f213ed5e154cf0819
Char-Al <charles.van.goethem@gmail.com>
2016-09-02 14:45:34 +0200

    add .gitignore

--- vcf_compare.pl
+++ vcf_compare.pl
@@ -30,7 +30,8 @@ my $pwd = getcwd();
 my @files;
 
 # Optional arguments
-my $output = $pwd."/";
+my $output = "";
+my $details;
 #my $makeGraphs = '';
 
 # General arguments
@@ -39,12 +40,13 @@ my $help 		= 0;
 my $verbosity	= 0;
 
 
-## Parse options and print usage if there is a syntax error, 
+## Parse options and print usage if there is a syntax error,
 ## or if usage was explicitly requested.
 
 GetOptions(
 	'f|files=s'	 		=> \@files,
 	'o|output=s'		=> \$output,
+	'd|details'			=> \$details,
 #	'g|makeGraphs'		=> \$makeGraphs,
 	'v|verbosity=i'		=> \$verbosity,
 	'help|?' 			=> \$help,
@@ -67,12 +69,30 @@ foreach my $file (@files) {
 	Hi, I dont understand...
 	The file '$file' does not exist !
 	Please check that !", "\n")) unless (-e $file);
-	
+
 	pod2usage(colored(['Red'], "Error :
 	Hi, I am sorry but there is somthing wrong...
 	The file '$file' is not a regulary file !
 	Can you verify this please.", "\n")) unless (-f $file);
 }
+
+# Check the output
+if ($output ne "") {
+	pod2usage(colored(['Red'], "Error :
+	There is something wrong...
+	The repertory '$output' does not exist !
+	You may create it before !", "\n")) unless (-e $output);
+
+	pod2usage(colored(['Red'], "Error :
+	I dont think it is normal...
+	The repertory '$output' is not a repertory !
+	I will cry if you dont fix this :'(", "\n")) unless (-d $output);
+} else {
+	$output = $pwd."/";
+	print STDERR colored(['Yellow'], "Warning : All the outputs will be create here :
+	$output","\n");
+}
+
 
 ##########################################################################################
 ##########################################################################################
@@ -85,14 +105,17 @@ foreach my $file (@files) {
 	open(VCF,$file) or die ("Error !\n");
 	#$INFOS{$file};
 	$file = basename($file);
+	my @all_lines;
 	while(<VCF>) {
 		chomp($_);
 		if(/^#/) {
 			if(/INFO=<ID=([^,]*).*Description="([^"]*).*/) {
-				$HEADER{$file}{"INFO"}{$1} = $2;
+				$HEADER{$file}{"INFO"}{$1}{"desc"} = $2;
+				$HEADER{$file}{"INFO"}{$1}{"line"} = $_;
 			}
 			if(/FORMAT=<ID=([^,]*).*Description="([^"]*).*/) {
-				$HEADER{$file}{"FORMAT"}{$1} = $2;
+				$HEADER{$file}{"FORMAT"}{$1}{"desc"} = $2;
+				$HEADER{$file}{"FORMAT"}{$1}{"line"} = $_;
 			}
 		} else {
 			my @line = split("\t");
@@ -100,35 +123,155 @@ foreach my $file (@files) {
 			#$VARIANTS{$file}{"$line[0]_$line[1]_$line[3]_$line[4]"} = 1 ;
 			push(@{$VARIANTS{"$line[0]_$line[1]_$line[3]_$line[4]"}{"files"}},$file) ;
 			$VARIANTS{"$line[0]_$line[1]_$line[3]_$line[4]"}{"lines"} = $_ ;
+			my %hash_line = parse_line($_);
+			#print Dumper \%hash_line;
+			push(@all_lines,\%hash_line);
 		}
 	}
+	print Dumper \@all_lines;
+	close(VCF);
 }
 
 ##########################################################################################
 ##########################################################################################
 
-my %COUNT_VARIANTS;
 
+my %TREAT_VARIANTS;
+my %VCF;
 foreach my $key (keys(%VARIANTS)){
 	if( @{$VARIANTS{$key}{"files"}} == 1) {
-		$COUNT_VARIANTS{${$VARIANTS{$key}{"files"}}[0]} += 1;
+		my $name = substr(${$VARIANTS{$key}{"files"}}[0],0,-4);
+		$TREAT_VARIANTS{$name}{"count"} += 1;
+		$TREAT_VARIANTS{$name}{"vcf"} .= $VARIANTS{$key}{"lines"}."\n";
 	} else {
 		my $name = "";
 		foreach my $names (@{$VARIANTS{$key}{"files"}}) {
-			$name .= $names."_AND_";
+			$name .= substr($names,0,-4)."_AND_";
 		}
 		$name = substr($name, 0, -5);
-		$COUNT_VARIANTS{$name} += 1;
-		print $VARIANTS{$key}{"lines"}."\n";
+		$TREAT_VARIANTS{$name}{"count"} += 1;
+		$TREAT_VARIANTS{$name}{"vcf"} .= $VARIANTS{$key}{"lines"}."\n";
 	}
 }
 
+# print Dumper \%HEADER;
+# print Dumper \%VARIANTS;
+# print Dumper \%TREAT_VARIANTS;
+
+#foreach my $key (keys(%TREAT_VARIANTS)) {
+#	open(FILE, ">$output$key") or die("Cannot open $output$key");
+#	print FILE $TREAT_VARIANTS{$key}{"vcf"};
+#	print $key." : ".$TREAT_VARIANTS{$key}{"count"}."\n";
+#}
+
+##########################################################################################
+##########################################################################################
 
 
+sub parse_line {
+	my $line = shift(@_);
+	my ($chr, $pos, $ID, $ref, $alt, $qual, $filter, $info, $format, @formats) = split("\t",$line);
+	my @infos = split(";",$info);
 
-#print Dumper \%HEADER;
-print Dumper \%VARIANTS;
-print Dumper \%COUNT_VARIANTS;
+	my %hash_line = (
+		"chr"	=> $chr,
+		"pos"	=> $pos,
+		"ID"	=> $ID,
+		"ref"	=> $ref,
+		"alt"	=> $alt,
+		"qual"	=> $qual,
+		"filter"	=> $filter
+	);
+	#print Dumper \%hash_line;
+
+	# Add each each formats
+	my $i=1;
+	my @all_formats = split(":",$format);
+	#$hash_line{"format"}{"format"} = $format;
+	foreach my $element (@formats) {
+		my @elements = split(":",$element);
+		for my $j (0..(@all_formats-1)){
+			$hash_line{"format"}{"indiv".$i}{$all_formats[$j]} = $elements[$j];
+		}
+		$i++;
+	}
+
+	# Add each infos
+	foreach my $value (@infos) {
+		my @values = split("=",$value);
+		$hash_line{"infos"}{$values[0]} = $values[1];
+	}
+
+	#print Dumper \%hash_line;
+	return %hash_line;
+}
 
 
+##########################################################################################
+##########################################################################################
 
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+compare_leon.pl - Compare Leon compression algorithm with gzip (generally used) for fastQ.
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+compare_leon.pl  -f file.fastq (-f file2.fastq,file3.fastq) -d directory/with/some/fastq
+
+=head1 DESCRIPTION
+
+This script compress and uncompress automatically some fastQ file.
+
+=head1 OPTIONS
+
+=head2 General
+
+	-h,--help		Print this help
+	-m,--man		Open man page
+	-v,--verbosity		Level of verbosity
+
+=head2 Mandatory arguments
+
+	-f,--file=file.fastq			Specify the fastq file you want use (possible multiple file)
+	-d,--directory=path/to/directory	Specify a directory contains some fastq you want use (possible multiple directory)
+
+=head2 Optional arguments
+
+	-o,--output=repertory			You can specify the output repertory (default Current)
+	-g,--makeGraphs					Generate automatically awesome graphs with R and ggplot2
+
+=head1 AUTHORS
+
+=over 4
+
+=item -
+Charles VAN GOETHEM
+
+=item -
+Pauline SARRAROLS
+
+                    /
+               ,.. /
+             ,'   ';
+  ,,.__    _,' /';  .
+ :','  ~~~~    '. '~
+:' (   )         )::,
+'. '. .=----=..-~  .;'
+ '  ;'  ::   ':.  '"
+   (:   ':    ;)
+    \\   '"  ./
+     '"      '"
+
+=back
+
+=cut
